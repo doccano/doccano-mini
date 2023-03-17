f983b74f4bea04b9a8f7696b1a26cb70f0212296
yasufumi <yasufumi.taniguchi@gmail.com>
2023-03-17 14:23:32 +0900

    Add a task free annotation

--- doccano_mini/app.py
+++ doccano_mini/app.py
@@ -1,10 +1,13 @@
+import pandas as pd
 import streamlit as st
 from langchain.chains import LLMChain
 from langchain.llms import OpenAI
+from langchain.prompts.few_shot import FewShotPromptTemplate
+from langchain.prompts.prompt import PromptTemplate
 
 from doccano_mini.examples import select_example_df
 from doccano_mini.prompts import select_prompt_maker
-from doccano_mini.tasks import options
+from doccano_mini.tasks import TaskType, options
 
 CODE = """from langchain.chains import load_chain
 
@@ -22,14 +25,7 @@ def display_download_button():
         )
 
 
-def main():
-    st.set_page_config(page_title="doccano-mini", page_icon=":memo:")
-
-    st.title("doccano-mini")
-
-    st.header("Select your task")
-    task = st.selectbox("", options=options, label_visibility="collapsed")
-
+def task_classification(task: TaskType):
     st.header("Annotate your data")
     df = select_example_df(task)
     edited_df = st.experimental_data_editor(df, num_rows="dynamic", width=1000)
@@ -64,7 +60,7 @@ def main():
         st.markdown(f"```\n{prompt.format(input=text)}\n```")
 
     if st.button("Predict"):
-        llm = OpenAI(model_name=model_name, temperature=temperature, top_p=top_p)
+        llm = OpenAI(model_name=model_name, temperature=temperature, top_p=top_p)  # type:ignore
         chain = LLMChain(llm=llm, prompt=prompt)
         response = chain.run(text)
         label = response.split(":")[1]
@@ -72,6 +68,66 @@ def main():
 
         chain.save("config.yaml")
         display_download_button()
+
+
+def main():
+    st.set_page_config(page_title="doccano-mini", page_icon=":memo:")
+
+    st.title("doccano-mini")
+
+    st.header("Select your task")
+    task = st.selectbox("", options=options, label_visibility="collapsed")
+
+    if task == "Text Classification":
+        task_classification(task)
+    else:
+        num_cols = st.number_input("Set the number of columns", min_value=2, max_value=10)
+        columns = [st.text_input(f"Column {i}:", value=f"column {i}") for i in range(1, num_cols + 1)]
+
+        df = pd.DataFrame({column: [] for column in columns}, dtype=str)
+        edited_df = st.experimental_data_editor(df, num_rows="dynamic", width=1000)
+
+        example_prompt = PromptTemplate(
+            input_variables=columns, template="\n".join([f"{column}: {{{column}}}" for column in columns])
+        )
+
+        prefix = st.text_area(
+            label="Enter task instruction", placeholder="Predict column 2 based on column 1", height=200
+        )
+        suffix = "\n".join([f"{column}: {{{column}}}" for column in columns[:-1]])
+
+        prompt = FewShotPromptTemplate(
+            examples=edited_df.to_dict(orient="records"),
+            example_prompt=example_prompt,
+            prefix=prefix,
+            suffix=suffix,
+            input_variables=columns[:-1],
+        )
+
+        inputs = [st.text_input(f"Input for {columns[i]}:") for i in range(num_cols - 1)]
+
+        st.markdown(f"```\n{prompt.format(**dict(zip(columns, inputs)))}\n```")
+
+        available_models = (
+            "gpt-3.5-turbo",
+            "gpt-3.5-turbo-0301",
+            "text-davinci-003",
+            "text-davinci-002",
+            "code-davinci-002",
+        )
+        # Use text-davinci-003 by default.
+        model_name = st.selectbox("Model", available_models, index=2)
+        temperature = st.slider("Temperature", min_value=0.0, max_value=1.0, value=0.7, step=0.01)
+        top_p = st.slider("Top-p", min_value=0.0, max_value=1.0, value=1.0, step=0.01)
+        if st.button("Predict"):
+            llm = OpenAI(model_name=model_name, temperature=temperature, top_p=top_p)  # type:ignore
+            chain = LLMChain(llm=llm, prompt=prompt)
+            response = chain.run(**dict(zip(columns, inputs)))
+            label = response.split(":")[1]
+            st.text(label)
+
+            chain.save("config.yaml")
+            display_download_button()
 
     st.header("Usage")
     st.code(CODE)
