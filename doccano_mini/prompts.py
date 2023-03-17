40bf21c9dd2f2d7a34636c23951a72fec464dd95
yasufumi <yasufumi.taniguchi@gmail.com>
2023-03-17 15:51:46 +0900

    Add make_task_free_prompt method

--- doccano_mini/prompts.py
+++ doccano_mini/prompts.py
@@ -24,7 +24,25 @@ def make_classification_prompt(examples: List[dict]) -> FewShotPromptTemplate:
     return prompt
 
 
+def make_task_free_prompt(examples: List[dict]) -> FewShotPromptTemplate:
+    columns = list(examples[0])
+
+    example_prompt = PromptTemplate(
+        input_variables=columns, template="\n".join([f"{column}: {{{column}}}" for column in columns])
+    )
+
+    prompt = FewShotPromptTemplate(
+        examples=examples,
+        example_prompt=example_prompt,
+        suffix="\n".join([f"{column}: {{{column}}}" for column in columns[:-1]]),
+        input_variables=columns[:-1],
+    )
+    return prompt
+
+
 def select_prompt_maker(task: TaskType):
     if task == TaskType.TEXT_CLASSIFICATION.value:
         return make_classification_prompt
+    elif task == TaskType.TASK_FREE.value:
+        return make_task_free_prompt
     raise ValueError(f"Task {task} is not supported.")
