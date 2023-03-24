85c7489e8e2fc3bbb11549f60d15a9351efe05b2
Hironsan <hiroki.nakayama.py@gmail.com>
2023-03-24 11:43:43 +0900

    Extract openai model form

--- doccano_mini/models.py
+++
@@ -1,8 +0,0 @@
-# https://platform.openai.com/docs/models/gpt-3-5
-AVAILABLE_MODELS = (
-    "gpt-3.5-turbo",
-    "gpt-3.5-turbo-0301",
-    "text-davinci-003",
-    "text-davinci-002",
-    "code-davinci-002",
-)
