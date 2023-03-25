4c7713ebdf46a27e70053d31b9c167da76c5a4d9
Hironsan <hiroki.nakayama.py@gmail.com>
2023-03-25 09:54:22 +0900

    Use BasePage in task pages

--- doccano_mini/pages/01_Text_Classification.py
+++ doccano_mini/pages/01_Text_Classification.py
@@ -1,42 +1,25 @@
-import streamlit as st
-from langchain.chains import LLMChain
+from typing import Dict, List
 
-from doccano_mini.components import (
-    display_download_button,
-    display_usage,
-    openai_model_form,
-    task_instruction_editor,
-)
+import streamlit as st
+
 from doccano_mini.examples import make_classification_example
+from doccano_mini.layout import BasePage
 from doccano_mini.prompts import make_classification_prompt
 
-st.title("Text Classification")
-st.header("Annotate your data")
-df = make_classification_example()
-edited_df = st.experimental_data_editor(df, num_rows="dynamic", width=1000)
-examples = edited_df.to_dict(orient="records")
 
-prompt = make_classification_prompt(examples)
-prompt = task_instruction_editor(prompt)
+class TextClassificationPage(BasePage):
+    def make_examples(self, columns: List[str]):
+        df = make_classification_example()
+        edited_df = st.experimental_data_editor(df, num_rows="dynamic", width=1000)
+        examples = edited_df.to_dict(orient="records")
+        return examples
 
-st.header("Test")
-col1, col2 = st.columns([3, 1])
-inputs = {"input": col1.text_area(label="Please enter your text.", value="", height=300)}
+    def make_prompt(self, examples: List[Dict]):
+        return make_classification_prompt(examples)
 
-with col2:
-    llm = openai_model_form()
+    def prepare_inputs(self, columns: List[str]):
+        return {"input": st.text_area(label="Please enter your text.", value="", height=300)}
 
-with st.expander("See your prompt"):
-    st.markdown(f"```\n{prompt.format(**inputs)}\n```")
 
-if llm is None:
-    st.error("Enter your API key.")
-
-if st.button("Predict", disabled=llm is None):
-    chain = LLMChain(llm=llm, prompt=prompt)  # type:ignore
-    response = chain.run(**inputs)
-    st.text(response)
-
-    chain.save("config.yaml")
-    display_download_button()
-display_usage()
+page = TextClassificationPage(title="Text Classification")
+page.render()
