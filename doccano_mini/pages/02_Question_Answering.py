0b7b5839a78d0e814c7293c081c0cb9fa7093190
Hironsan <hiroki.nakayama.py@gmail.com>
2023-03-27 08:52:36 +0900

    Add a question answering page

---
+++ doccano_mini/pages/02_Question_Answering.py
@@ -0,0 +1,28 @@
+from typing import Dict, List
+
+import streamlit as st
+
+from doccano_mini.examples import make_question_answering_example
+from doccano_mini.layout import BasePage
+from doccano_mini.prompts import make_question_answering_prompt
+
+
+class QuestionAnsweringPage(BasePage):
+    def make_examples(self, columns: List[str]):
+        df = make_question_answering_example()
+        edited_df = st.experimental_data_editor(df, num_rows="dynamic", width=1000)
+        examples = edited_df.to_dict(orient="records")
+        return examples
+
+    def make_prompt(self, examples: List[Dict]):
+        return make_question_answering_prompt(examples)
+
+    def prepare_inputs(self, columns: List[str]):
+        return {
+            "context": st.text_area(label="Context.", value="", height=300),
+            "question": st.text_area(label="Question.", value="", height=100),
+        }
+
+
+page = QuestionAnsweringPage(title="Question Answering")
+page.render()
